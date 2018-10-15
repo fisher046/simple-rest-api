cfc00f12316f7f955423fce558804b475bfe2306
huanzhang <huanzhang@freewheel.tv>
2018-10-15 21:17:23 +0800

    Init

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"net/http"
+)
+
+const (
+	locationKey = "location"
+)
+
+// Response is a sample API response
+func Response(w http.ResponseWriter, r *http.Request) {
+	if r.Header.Get(locationKey) == "" {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("cannot know it come from"))
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("it comes from " + r.Header.Get(locationKey)))
+	return
+}
+
+func main() {
+	http.HandleFunc("/cat", Response)
+	http.ListenAndServe(":30080", nil)
+}
